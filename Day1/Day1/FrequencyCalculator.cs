35c5725c4754712bb8614e82a861a13109694bf8
Tomas Oplatek <oplatek.t@gmail.com>
2018-12-01 19:07:24 +0000

    Day 1 solution

---
+++ Day1/Day1/FrequencyCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    public class FrequencyCalculator
+    {
+        private const string FileName = "PuzzleInput.txt";
+
+        public static int CalculateFrequency()
+        {
+            var calculatedFrequency = 0;
+
+            var lines = GetLines();
+            foreach (var line in lines)
+            {
+                calculatedFrequency += GetNumber(line);
+            }
+
+            return calculatedFrequency;
+        }
+
+        public static int CalculateFrequencyReachedTwice()
+        {
+            var reachedFrequencies = new HashSet<int> { 0 };
+            var frequencyReachedTwice = false;
+            var lines = GetLines().ToList();
+            var calculatedFrequency = 0;
+
+            while (!frequencyReachedTwice)
+            {
+                foreach (var line in lines)
+                {
+                    calculatedFrequency += GetNumber(line);
+
+                    if (reachedFrequencies.Contains(calculatedFrequency))
+                    {
+                        frequencyReachedTwice = true;
+                        break;
+                    }
+                    else
+                    {
+                        reachedFrequencies.Add(calculatedFrequency);
+                    }
+                }
+            }
+
+            return calculatedFrequency;
+        }
+
+        private static int GetNumber(string line)
+        {
+            var number = int.Parse(line.Substring(1));
+
+            return line[0] == '+' ? number : -number;
+        }
+
+        private static IEnumerable<string> GetLines()
+        {
+            var inputPath = Path.Combine(Environment.CurrentDirectory, @"Data\", FileName);
+
+            var lines = File.ReadLines(inputPath);
+
+            return lines;
+        }
+    }
+}
---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var frequency = FrequencyCalculator.CalculateFrequency();
+            var frequencyReachedTwice = FrequencyCalculator.CalculateFrequencyReachedTwice();
+            Console.WriteLine($"Resulting frequency is: {frequency}");
+            Console.WriteLine($"First frequency reached twice is: {frequencyReachedTwice}");
+            Console.ReadKey();
+        }
+    }
+}
