e1a28cb82c7e5e5e2071bd47aaeb0fcae31e3c8e
Tomas Oplatek <oplatek.t@gmail.com>
2018-12-03 19:42:15 +0000

    Day 3 part 2

--- AdventOfCode2018/Day3/ClaimCalculator.cs
+++ AdventOfCode2018/Day3/ClaimCalculator.cs
@@ -7,57 +7,88 @@ namespace Day3
     public static class ClaimCalculator
     {
         private const string DirectoryName = "Day3";
-        private static HashSet<int>[,] _fabricArrayWithClaims;
+        private static HashSet<int>[,] _fabricArrayWithClaimIds;
+        private static List<Claim> _claims;
 
         public static int OverlappingFabricInches()
         {
             var fabricArray = GetFabricArrayWithClaimIds();
 
-            return fabricArray.Cast<HashSet<int>>().Count(i => i.Count > 1);
+            return fabricArray.Cast<HashSet<int>>().Count(i => i != null && i.Count > 1);
         }
 
         public static int GetClaimWithoutOverlap()
         {
             var fabricArray = GetFabricArrayWithClaimIds();
 
-            fabricArray.Cast<HashSet<int>>().Where(i => i.Count > 1);
+            var idsWithoutOverlap = fabricArray.Cast<HashSet<int>>().Where(i => i != null && i.Count == 1);
+
+            var uniqueIds = new HashSet<int>();
+            foreach (var ids in idsWithoutOverlap)
+            {
+                uniqueIds.UnionWith(ids);
+            }
+
+            foreach (var uniqueId in uniqueIds)
+            {
+                var claim = _claims.First(c => c.Id == uniqueId);
+                var isOverlapped = false;
+
+                for (var i = 0; i < claim.Width; i++)
+                {
+                    for (var j = 0; j < claim.Height; j++)
+                    {
+                        isOverlapped = fabricArray[i + claim.LeftEdge, j + claim.TopEdge].Count > 1;
+                        if (isOverlapped) break;
+                    }
+
+                    if (isOverlapped) break;
+                }
+
+                if (!isOverlapped)
+                {
+                    return claim.Id;
+                }
+            }
+
+            return -1;
         }
 
         private static HashSet<int>[,] GetFabricArrayWithClaimIds()
         {
-            if (_fabricArrayWithClaims == null)
+            if (_fabricArrayWithClaimIds == null)
             {
                 var lines = FileHelper.GetLines(DirectoryName);
-                var claims = lines.Select(l => new Claim(l)).ToList();
+                _claims = lines.Select(l => new Claim(l)).ToList();
 
-                var arrayWidth = claims.Max(c => c.LeftEdge + c.Width);
-                var arrayHeight = claims.Max(c => c.TopEdge + c.Height);
+                var arrayWidth = _claims.Max(c => c.LeftEdge + c.Width);
+                var arrayHeight = _claims.Max(c => c.TopEdge + c.Height);
 
                 var fabricArray = new HashSet<int>[arrayWidth, arrayHeight];
 
-                for (var i = 0; i < arrayWidth; i++)
-                {
-                    for (var j = 0; j < arrayHeight; j++)
-                    {
-                        fabricArray[i, j] = new HashSet<int>();
-                    }
-                }
-
-                foreach (var claim in claims)
+                foreach (var claim in _claims)
                 {
                     for (var i = 0; i < claim.Width; i++)
                     {
                         for (var j = 0; j < claim.Height; j++)
                         {
-                            fabricArray[i + claim.LeftEdge, j + claim.TopEdge].Add(claim.Id);
+                            var horizontalIndex = i + claim.LeftEdge;
+                            var verticalIndex = j + claim.TopEdge;
+
+                            if (fabricArray[horizontalIndex, verticalIndex] == null)
+                            {
+                                fabricArray[horizontalIndex, verticalIndex] = new HashSet<int>();
+                            }
+
+                            fabricArray[horizontalIndex, verticalIndex].Add(claim.Id);
                         }
                     }
                 }
 
-                _fabricArrayWithClaims = fabricArray;
+                _fabricArrayWithClaimIds = fabricArray;
             }
             
-            return _fabricArrayWithClaims;
+            return _fabricArrayWithClaimIds;
         }
     }
 }
--- AdventOfCode2018/Day3/Program.cs
+++ AdventOfCode2018/Day3/Program.cs
@@ -7,7 +7,9 @@ namespace Day3
         static void Main(string[] args)
         {
             var overlap = ClaimCalculator.OverlappingFabricInches();
+            var withoutOverlapId = ClaimCalculator.GetClaimWithoutOverlap();
             Console.WriteLine($"Square inches of fabric that are within two or more claims: {overlap}");
+            Console.WriteLine($"ID of the only claim that doesn't overlap: {withoutOverlapId}");
             Console.ReadKey();
         }
     }
