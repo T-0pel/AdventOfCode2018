5d0763f76771d257a82213608515970854cc5198
Tomas Oplatek <oplatek.t@gmail.com>
2018-12-05 08:50:55 +0000

    Day 5

---
+++ AdventOfCode2018/Day5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SharedLibrary;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = FileHelper.GetLines("Day5").First();
+
+            var isDone = false;
+            var previousLetter = '-';
+            var resultString = line;
+            var indexesToDelete = new HashSet<int>();
+            while (!isDone)
+            {
+                indexesToDelete.Clear();
+                var changeInCycle = false;
+                var index = 0;
+                foreach (var letter in line)
+                {
+                    if (string.Equals(previousLetter.ToString(), letter.ToString(), StringComparison.CurrentCultureIgnoreCase) &&
+                        char.IsUpper(previousLetter) == char.IsLower(letter))
+                    {
+                        changeInCycle = true;
+                        indexesToDelete.Add(index);
+                    }
+
+                    previousLetter = letter;
+                    index++;
+                }
+
+                foreach (var i in indexesToDelete.OrderByDescending(ii => ii))
+                {
+                    resultString = resultString.Remove(i, 2);
+                }
+
+                line = resultString;
+                isDone = !changeInCycle;
+            }
+
+            Console.WriteLine(resultString);
+            Console.WriteLine(resultString.Length);
+            Console.ReadKey();
+        }
+    }
+}
