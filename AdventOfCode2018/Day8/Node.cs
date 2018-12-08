043530d2bdb24c75bc1e7c54b8ee7379b25357e9
Tomas Oplatek <oplatek.t@gmail.com>
2018-12-08 10:39:10 +0000

    Day 8

---
+++ AdventOfCode2018/Day8/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day8
+{
+    public class Node
+    {
+        public int Id { get; }
+        public List<Node> ChildNodes { get; set; }
+        public List<int> Metadata { get; set; }
+
+        private static int _idCounter;
+
+        public Node()
+        {
+            Id = ++_idCounter;
+            ChildNodes = new List<Node>();
+            Metadata = new List<int>();
+        }
+
+        public int GetNodeValue()
+        {
+            var value = 0;
+            if (ChildNodes.Count == 0)
+            {
+                value = Metadata.Sum();
+            }
+            else
+            {
+                foreach (var meta in Metadata)
+                {
+                    var index = meta - 1;
+                    var node = ChildNodes.ElementAtOrDefault(index);
+                    if (node != null)
+                    {
+                        value += node.GetNodeValue();
+                    }
+                }
+            }
+
+            return value;
+        }
+    }
+}
---
+++ AdventOfCode2018/Day8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SharedLibrary;
+
+namespace Day8
+{
+    class Program
+    {
+        private static List<Node> _nodes = new List<Node>();
+
+        static void Main(string[] args)
+        {
+            var lines = FileHelper.GetLines("Day8").ToList();
+            var split = lines[0].Split(' ');
+            ParseNodeData(split, null);
+            Console.WriteLine(_nodes.Sum(n => n.Metadata.Sum()));
+            Console.WriteLine(_nodes[0].GetNodeValue());
+            Console.ReadKey();
+        }
+
+        private static int ParseNodeData(IReadOnlyList<string> data, Node parentNode)
+        {
+            var node = new Node();
+            _nodes.Add(node);
+
+            parentNode?.ChildNodes.Add(node);
+
+            var childNodesCount = int.Parse(data[0]);
+            var metadataCount = int.Parse(data[1]);
+
+            var metadataIndex = 2;
+            for (var i = 0; i < childNodesCount; i++)
+            {
+                metadataIndex += ParseNodeData(data.TakeLast(data.Count - metadataIndex).ToArray(), node);
+            }
+
+            for (var i = 0; i < metadataCount; i++)
+            {
+                node.Metadata.Add(int.Parse(data[metadataIndex++]));
+            }
+
+            return metadataIndex;
+        }
+    }
+}
