bd0426d4970439ea96a515466bf3bce433bf3335
Tomas Oplatek <oplatek.t@gmail.com>
2018-12-10 19:00:46 +0000

    Day 10 full solution

---
+++ AdventOfCode2018/Day10/Point.cs
@@ -0,0 +1,25 @@
+﻿namespace Day10
+{
+    public class Point
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int VelocityX { get; }
+        public int VelocityY { get; }
+
+        public Point(string line)
+        {
+            var spaceSplit = line.Split(',');
+            PositionX = int.Parse(spaceSplit[0].Substring(spaceSplit[0].Length - 6, 6).Trim());
+            PositionY = int.Parse(spaceSplit[1].Substring(1, 6).Trim());
+            VelocityX = int.Parse(spaceSplit[1].Substring(spaceSplit[1].Length - 2, 2).Trim());
+            VelocityY = int.Parse(spaceSplit[2].Substring(1, 2).Trim());
+        }
+
+        public void Move()
+        {
+            PositionX += VelocityX;
+            PositionY += VelocityY;
+        }
+    }
+}
---
+++ AdventOfCode2018/Day10/Program.cs
@@ -0,0 +1,69 @@
+﻿using SharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day10
+{
+    class Program
+    {
+        private static List<Point> _points = new List<Point>();
+
+        static void Main(string[] args)
+        {
+            var lines = FileHelper.GetLines("Day10");
+            _points = lines.Select(l => new Point(l)).ToList();
+
+            var seconds = 0;
+            while (true)
+            {
+                const int yLimitPoint = 10;
+
+                var yDifference = _points.Max(p => p.PositionY) - _points.Min(p => p.PositionY);
+
+                if (yDifference < yLimitPoint)
+                {
+                    // Shift to take care of potential negative values
+                    var centre = 500;
+
+                    var grid = new char[_points.Max(p => p.PositionX) + centre + 1, _points.Max(p => p.PositionY) + centre + 1];
+                    foreach (var point in _points)
+                    {
+                        grid[point.PositionX + centre, point.PositionY + centre] = '#';
+                    }
+
+                    Console.WriteLine();
+                    for (var y = _points.Min(p => p.PositionY); y < _points.Max(p => p.PositionY) + 1; y++)
+                    {
+                        for (var x = _points.Min(p => p.PositionX); x < _points.Max(p => p.PositionX) + 1; x++)
+                        {
+                            Console.Write(grid[x + centre, y + centre] == '#' ? '#' : '.');
+                        }
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine($"Elapsed seconds: {seconds}");
+                    break;
+                }
+
+                foreach (var point in _points)
+                {
+                    point.Move();
+                }
+
+                seconds++;
+
+                //for (int x = 0; x < gridSize; x++)
+                //{
+                //    for (int y = 0; y < gridSize; y++)
+                //    {
+                //        Console.Write(grid[x, y] == '#' ? '#' : '.');
+                //    }
+                //    Console.WriteLine();
+                //}
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
